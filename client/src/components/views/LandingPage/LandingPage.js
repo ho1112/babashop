fe697d4130bd467566600814c40975ede5d8b106
unknown <carrien1112@gmail.com>
2020-06-15 22:33:48 +0900

    add load more function by mongoDB skip,limit

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,19 +8,48 @@ import ImageSlider from '../../utils/ImageSlider'
 function LandingPage() {
 
     const [Products, setProducts] = useState([])
+    const [Skip, setSkip] = useState(0)
+    const [Limit, setLimit] = useState(8) //default image count:8
+    const [PostSize, setPostSize] = useState()
 
     useEffect(() => {
-        axios.post('/api/product/products')
+        let body = {
+            skip: Skip,
+            limit: Limit
+        }
+        getProducts(body)
+    }, [])
+    
+    const getProducts = (body) => {
+        //상품정보 가져오기
+        axios.post('/api/product/products', body)
             .then(response => {
                 if(response.data.success) {
-                    setProducts(response.data.productInfo)
+                    if(body.loadMore) { //더보기일 경우
+                        setProducts([...Products, ...response.data.productInfo]) //Products(기존 상품)+response(새로 가져온 상품)
+                    } else {
+                        setProducts(response.data.productInfo)
+                    } 
+                    setPostSize(response.data.postSize) //더보기할 상품이 남아있는지 상품 갯수를 넣어준다
                 }else {
                     alert("failed Loading")
                 }
             })
-    }, [])
+    }
 
-    ////ant design - Carousel로 이미지 슬라이더 구현
+    //더보기
+    const loadMoreHandler = () => {
+        let skip = Skip + Limit //기존 스킵+리미트
+        let body = {
+            skip: skip,
+            limit: Limit,
+            loadMore: true
+        }
+        getProducts(body)
+        setSkip(skip)//스킵값 보존
+    }
+
+    //ant design - Carousel로 이미지 슬라이더 구현
     const renderCards = Products.map((product, index) =>{
         return <Col lg={6} md={8} xs={24} key={index}>
             <Card
@@ -33,6 +62,7 @@ function LandingPage() {
             </Card>
         </Col>
     })
+
 
 
     return (
@@ -49,10 +79,12 @@ function LandingPage() {
                 {renderCards}
             </Row>
         
+            {PostSize >= Limit && /* 가져온 상품 갯수가 limit보다 크거나 같으면 더보기 버튼 표시 */
+                <div style={{ display:'flex', justifyContent: 'center'}}>
+                    <button onClick={loadMoreHandler}>더보기</button>
+                </div>
+            }
 
-            <div style={{ display:'flex', justifyContent: 'center'}}>
-                <button>더보기</button>
-            </div>
         </div>
     )
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -39,11 +39,19 @@ router.post('/', (req, res) => {
 //Landing page(top)
 router.post('/products', (req, res) => {
   // product collection에 들어있는 모든 상품정보 가져오기
+  let limit = req.body.limit ? parseInt(req.body.limit) : 20;
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
   Product.find()   //Product 모델로 mongo DB의 Product를 전부 가져온다
     .populate("writer") //.populate()로 writer(상품을 업로드한 ID)와 관련된 모든 정보를 가져온다
+    .skip(skip) //몇번부터 가져올 것인지 skip~ limit만큼
+    .limit(limit) //가져올 갯수
     .exec((err, productInfo) => {
       if(err) return res.status(400).json( {success: false, err} )
-      return res.status(200).json( {success: true, productInfo } )
+      return res.status(200).json( {
+        success: true, productInfo,
+        postSize: productInfo.length //상품 갯수
+      })
     })
 })
 
